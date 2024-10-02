b769df08c8cc34bfc0edec14efae195b1acd7998
eddafir_Mohamed_Amine404 <eddafir.mohamed.amine01@gmail.com>
2024-10-02 14:13:33 +0100

    Fix 500 error in GET method by removing @Lob annotation from model

--- src/main/java/com/secure/notes/models/Notes.java
+++ src/main/java/com/secure/notes/models/Notes.java
@@ -2,22 +2,30 @@ package com.secure.notes.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Objects;
 
-@Setter
-@Getter
 @Entity
 @Data
 public class Notes {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO) // @GeneratedValue to indicate that the ID should be generated automatically.
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notes_seq") // using GenerationType.SEQUENCE might be more consistent across various database implementations
+    @SequenceGenerator(name = "notes_seq", sequenceName = "notes_seq", allocationSize = 1)
     private Long id;
-    @Lob
+
+    /*
+        The @Lob annotation is used to store large objects,
+        either BLOB (binary large object) or CLOB (character large object).
+        Since your content field is a string, it’s mapped to a CLOB.
+        However, depending on the database you’re using,
+        @Lob may require a columnDefinition to specify how the text should be stored
+    */
+
+    @Column(name = "content", columnDefinition = "TEXT")
     private String content;
+
+    @Column(nullable = false)
     private String username;
 
     public Notes() {}
@@ -25,6 +33,30 @@ public class Notes {
         this.id = id;
         this.username = username;
         this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
