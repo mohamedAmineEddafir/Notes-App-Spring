a883d657ea93b90f58c677c5aed0ab680bcf3ea8
eddafir_Mohamed_Amine404 <eddafir.mohamed.amine01@gmail.com>
2024-10-06 22:49:03 +0100

    use JdbcUserDetails to stored in my user's in database with encoder password

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -1,17 +1,19 @@
 package com.secure.notes.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,7 +24,7 @@ public class SecurityConfig {
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
+    public SecurityConfig(PasswordEncoder passwordEncoder, DataSource dataSource) {
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -34,24 +36,33 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    // Method to add users after JdbcUserDetailsManager is initialized
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.builder()
-                    .username("user")
-                    .password(passwordEncoder.encode("user1"))
-                    .roles("USER")
-                    .build();
-        UserDetails admin =
-                User.builder()
-                    .username("admin")
-                    .password(passwordEncoder.encode("admin1"))
-                    .roles("ADMIN")
-                    .build();
-
-        // outputs {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy. fqvM/ BG
-        System. out. println(user. getPassword());
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager userManager = new JdbcUserDetailsManager(dataSource);
+        if (!userManager.userExists("user")){
+            userManager.createUser(
+                    User.withUsername("amine")
+                    .password(passwordEncoder.encode("123"))
+                    .authorities("ROLE_USER")
+                    .disabled(true)
+                    .build()
+            );
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+        if (!userManager.userExists("admin")){
+            userManager.createUser(
+                    User.withUsername("admin")
+                    .password(passwordEncoder.encode("@dmin"))
+                    .authorities("ROLE_ADMIN")
+                    .disabled(true)
+                    .build()
+            );
+        } else {
+            throw new UsernameNotFoundException("Admin not found");
+        }
+        return userManager;
     }
 }
